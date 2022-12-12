612ab773d7304485a9765ceb7b248083b64eda98
Amanda Ross <amikaross@gmail.com>
2022-12-12 11:24:25 -0700

    Delete cassettes, add new application.yml with auth key, update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   validates_presence_of :name 
   validates_presence_of :email 
   validates_uniqueness_of :email 
+  validates_presence_of :password_digest
+
+  has_secure_password
 
   def users_parties
     viewing_parties
