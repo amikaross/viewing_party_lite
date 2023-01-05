d8260fb2c3c36cee154dfed2078df7bdecc8463d
Amanda Ross <amikaross@gmail.com>
2023-01-05 11:50:56 -0700

    Remove comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
   validates_presence_of :name 
   validates_presence_of :email 
   validates_uniqueness_of :email 
-  # validates :password, presence: true, confirmation: true, on: :create
-  # validates :password_confirmation, presence: true , on: :create
-  # validates :password, presence: true
+
   has_secure_password
 
   def users_parties
